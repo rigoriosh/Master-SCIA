de9e87840856af42544cb47e97db138b0425dda6
raskarlitos1 <55096600+raskarlitos1@users.noreply.github.com>
2021-08-31 19:36:55 -0500

    Update ViewVariables.js
    Corrección integración código

--- src/paginas/unaActividad/views/ViewVariables.js
+++ src/paginas/unaActividad/views/ViewVariables.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import React, { useState, useEffect } from 'react'
 
 export const ViewVariables = (props) => {
     const variables = props.apiActividadVariable
